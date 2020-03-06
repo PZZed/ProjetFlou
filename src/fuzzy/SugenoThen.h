ba9bd5c9120ca2ddbcb5aec092528fc0e0fd682f
MaximeJumeaux <jumeaux.maxime@outlook.fr>
2020-03-06 11:07:27 +0100

    SugenoThen

--- src/fuzzy/SugenoThen.h
+++ src/fuzzy/SugenoThen.h
@@ -13,15 +13,25 @@
 namespace fuzzy{
     template <class T>
     class SugenoThen : public Then<T> {
-
+    private:
+        T cache;
+    public:
+        SugenoThen(){}
+        virtual ~SugenoThen(){}
         T evaluate(core::Expression<T>* l,core::Expression<T>* r);
+        T getCache() const;
     };
 
     template<class T>
     T SugenoThen<T>::evaluate(core::Expression<T>* l, core::Expression<T>* r) {
-        return nullptr;
+        cache = l->evaluate();
+        return cache * r->evaluate();
     }
 
+    template<class T>
+    T SugenoThen<T>::getCache() const {
+        return cache;
+    }
 
 }
 
