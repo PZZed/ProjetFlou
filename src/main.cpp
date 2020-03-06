1ab8869732573f1a5bb47af9da8e1d45f7b4d514
lesliecaron <lesliecaron@live.fr>
2020-03-06 10:22:22 +0100

    ISbell

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "fuzzy/AndMin.h"
 #include "fuzzy/IsTriangle.h"
 #include "fuzzy/IsGaussian.h"
+#include "fuzzy/IsBell.h"
 
 using namespace core;
 using namespace std;
@@ -13,9 +14,10 @@ using namespace fuzzy;
 
 int main() {
 
-
+    IsBell<float> bell(0,1,2);
     IsGaussian<float> gaussian(0,1);
     ValueModel<float> m(12.f);
-    gaussian.evaluate(&m);
+    
+    cout << bell.evaluate(&m);
     return 0;
 }
