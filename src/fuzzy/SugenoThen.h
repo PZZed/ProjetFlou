9b05ca49ba1206d04e031dc36fb42ac900e510ea
PZZed <nicolas.herr9812@gmail.com>
2020-05-03 14:57:29 +0200

    SugenoThen

--- src/fuzzy/SugenoThen.h
+++ src/fuzzy/SugenoThen.h
@@ -13,13 +13,31 @@
 namespace fuzzy{
     template <class T>
     class SugenoThen : public Then<T> {
-
-        T evaluate(core::Expression<T>* l,core::Expression<T>* r);
+    public:
+        virtual T evaluate(core::Expression<T>* l,core::Expression<T>* r)const ;
+        virtual T getPremisse()const;
+        virtual void setPremisse(T);
+    private:
+        mutable T valeurPremisse; // mutable car modification dans le const.
     };
 
     template<class T>
-    T SugenoThen<T>::evaluate(core::Expression<T>* l, core::Expression<T>* r) {
-        return nullptr;
+    T SugenoThen<T>::evaluate(core::Expression<T>* l, core::Expression<T>* r) const{
+        if(l==nullptr||r== nullptr){
+            //pblm TODO exception ou un truc du genre
+        }
+        setPremisse(l->evaluate());
+        return this->valeurPremisse*r->evaluate();
+    }
+
+    template<class T>
+    T SugenoThen<T>::getPremisse() const {
+        return this->valeurPremisse;
+    }
+
+    template<class T>
+    void SugenoThen<T>::setPremisse(T premisse) {
+        this->valeurPremisse = premisse;
     }
 
 
