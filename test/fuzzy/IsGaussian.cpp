b3b178c8660d0f6107b65183beb95d676fb915ec
lesliecaron <lesliecaron@live.fr>
2020-05-04 15:11:21 +0200

    isGaussian

--- test/fuzzy/IsGaussian.cpp
+++ test/fuzzy/IsGaussian.cpp
@@ -8,12 +8,12 @@
 TEST(IsGaussian, evaluateFloat){
     ValueModel<float> v(1.0f);
     IsGaussian<float> op(1.0f, 1.0f);
-    ASSERT_EQ(op.evaluate(&v), 0.799178f);
+    ASSERT_EQ(op.evaluate(&v), 0.176033);
 
 }
 
 TEST(IsGaussian, evaluateDouble){
     ValueModel<double> v(1.0);
     IsGaussian<double> op(2.0, 2.0);
-    ASSERT_EQ(op.evaluate(&v), 0.799178);
+    ASSERT_EQ(op.evaluate(&v), 0.176033);
 }
