71cd524d7afe1d3d3ce72276a83c0e1a8d052207
lesliecaron <lesliecaron@live.fr>
2020-05-04 17:53:31 +0200

    Refactor

---
+++ src/calculate/File.h
@@ -0,0 +1,8 @@
+//
+// Created by pisel on 04/05/2020.
+//
+
+#ifndef PROJETFLOU_FILE_H
+#define PROJETFLOU_FILE_H
+
+#endif //PROJETFLOU_FILE_H
--- src/calculate/Variables.h
+++ src/calculate/Variables.h
@@ -5,6 +5,12 @@
 #ifndef PROJETFLOU_VARIABLES_H
 #define PROJETFLOU_VARIABLES_H
 
+namespace calculate{
+    <T>
+    class Variable
+}
+
+
 #endif //PROJETFLOU_VARIABLES_H
 // creer une map , avec des accesseur et des seteur
 //creer des variables
