5f7ff535a2f6a3ad97a9f9c2a3e21579d459fa8f
PZZed <nicolas.herr9812@gmail.com>
2020-05-04 18:07:15 +0200

    correction nary

---
+++ test/core/NaryShadowExpressionTest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by pz on 04/05/2020.
+//
+
+#include <gtest/gtest.h>
+#include "../../src/core/NaryShadowExpression.h"
+
+using namespace core;
+
+TEST(NaryShadowExpression,NaryShadowExpression_Constructor_Test){
+    NaryShadowExpression<float> naryShadowExpression;
+}
